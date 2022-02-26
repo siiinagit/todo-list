83d711a494c64b3616aa0e2b66752facb13b87ab
siiinagit <siiinagamedebate@gmail.com>
2022-02-26 23:16:18 +0100

    changes2

--- src/components/Form.js
+++ src/components/Form.js
@@ -46,7 +46,7 @@ const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
     <form onSubmit={onFormSubmit} style={{ backgroundColor: "#fffaed" }}>
       <TextField
         type="text"
-        label="enter the todo..."
+        label="enter the todo... list"
         InputLabelProps={{ required: false }}
         variant="filled"
         value={input}
